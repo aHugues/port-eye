e7526028e7d768ecaad58e426db8a3ec57cc5043
Aurélien Hugues <hugues.aurelien@bcgplatinion.com>
2019-08-19 16:08:51 +0200

    Setup Python project

--- setup.py
+++ setup.py
@@ -1,3 +1,26 @@
 from setuptools import setup
+from pathlib import Path
 
-setup()
+with open(str(Path(".") / "README.md"), "r", encoding="utf-8") as f:
+    README = f.read()
+
+setup(
+    name="port-eye",
+    version="0.0",
+    license="MIT",
+    url="https://github.com/aHugues/port-eye.git",
+    description="Simple CLI port scanner",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Aurélien Hugues",
+    author_email="me@aurelienhugues.com",
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "codecov",
+            "black",
+            "pylint",
+        ]
+    }
+)
