c71e6598c992bd0a09fa66ccc71c5778ec267d74
Aurélien Hugues <hugues.aurelien@bcgplatinion.com>
2019-08-19 16:30:55 +0200

    Correct travis deployment

--- setup.py
+++ setup.py
@@ -24,5 +24,19 @@ setup(
             "pylint",
         ]
     },
+    python_requires=">=2.7",
     entry_points={"console_scripts": ["port-eye=port_eye.main:main"]},
+       classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
