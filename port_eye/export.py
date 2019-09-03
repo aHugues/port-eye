378dc80c668da7e754b1f59e7ebab95be4e6855d
Aurélien Hugues <hugues.aurelien@bcgplatinion.com>
2019-09-03 12:06:46 +0200

    add basic support for html export

---
+++ port_eye/export.py
@@ -0,0 +1,29 @@
+"""Export functions to HTML."""
+
+from jinja2 import Environment, PackageLoader, select_autoescape
+import datetime
+
+class Export():
+
+    def __init__(self):
+        self.env = Environment(
+            loader=PackageLoader('port_eye', 'templates'),
+            autoescape=select_autoescape(['html'])
+        )
+        self.template = self.env.get_template('export.html.j2')
+    
+    def load_style(self):
+        style_template = self.env.get_template('style.css')
+        return style_template.render()
+
+    def render(self):
+        style = self.load_style()
+        today_date = datetime.datetime.now().strftime('%Y-%m-%d (%H:%M)')
+        duration = 13
+
+        with open('export.html', 'w') as outfile:
+            outfile.write(self.template.render(
+                style=style,
+                date=today_date,
+                duration=duration,
+            ))
