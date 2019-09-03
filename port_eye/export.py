e1be17535024760a791ffcd21e4208a766490730
Aurélien Hugues <hugues.aurelien@bcgplatinion.com>
2019-09-03 16:51:46 +0200

    add unit tests for the exporting

--- port_eye/export.py
+++ port_eye/export.py
@@ -3,43 +3,37 @@
 from jinja2 import Environment, PackageLoader, select_autoescape
 import datetime
 
-class Export():
+
+class Export:
 
     def __init__(self):
+        """Create a new Export element."""
         self.env = Environment(
             loader=PackageLoader('port_eye', 'templates'),
             autoescape=select_autoescape(['html'])
         )
         self.template = self.env.get_template('export.html.j2')
-    
+
     def load_style(self):
+        """Load style file and return it as string."""
         style_template = self.env.get_template('style.css')
         return style_template.render()
 
-    def render(self):
+    def render(self, report, path):
+        """Render the result report into the given path as an HTML file.
+
+        # Arguments
+        report (Report): Report object providing results to store
+        path (str): Path of the output html file
+        """
+
         style = self.load_style()
         today_date = datetime.datetime.now().strftime('%Y-%m-%d (%H:%M)')
-        duration = 13
+        new_report = report
+        new_report.date = today_date
 
-        hosts_status = [
-            {'ip': '192.168.0.1', 'status': 'up'},
-            {'ip': '192.168.0.1', 'status': 'up'},
-            {'ip': '192.168.0.1', 'status': 'up'},
-            {'ip': '192.168.0.1', 'status': 'down'},
-            {'ip': '192.168.0.1', 'status': 'up'},
-            {'ip': '192.168.0.1', 'status': 'up'},
-            {'ip': '192.168.0.1', 'status': 'up'},
-            {'ip': '192.168.0.1', 'status': 'down'},
-            {'ip': '192.168.0.1', 'status': 'up'},
-            {'ip': '192.168.0.1', 'status': 'up'},
-            {'ip': '192.168.0.1', 'status': 'up'},
-            {'ip': '192.168.0.1', 'status': 'up'},
-        ]
-
-        with open('export.html', 'w') as outfile:
+        with open(path, 'w') as outfile:
             outfile.write(self.template.render(
                 style=style,
-                date=today_date,
-                duration=duration,
-                hosts_status=hosts_status
+                report=report
             ))
---
+++ port_eye/report.py
@@ -0,0 +1,8 @@
+"""Handle complete data for report."""
+
+
+class Report:
+
+    def __init__(self):
+        pass 
+
