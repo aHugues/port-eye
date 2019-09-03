a8396303bb1988fd6a5a247320484e85f7da4693
Aurélien Hugues <hugues.aurelien@bcgplatinion.com>
2019-09-03 11:11:15 +0200

    Add Jinja to requirements

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     install_requires=version_specific_packages+[
         "click>=7",
+        "jinja2>=2.10",
     ],
     extras_require={
         "dev": [
